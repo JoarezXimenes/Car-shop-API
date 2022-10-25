b86d44abca4e133de8d6cd2b72ada82efaa0e30d
Joarez Ximenes <joarezximenesleitejunior@gmail.com>
2022-10-25 20:58:53 -0300

    IVehicle interface created

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+export const vehicleZodSchema = z.object({
+  model: z.string({
+    required_error: 'Model is required',
+    invalid_type_error: 'Model must be a string',
+  }).min(3, { message: 'Model must be 3 or more characters long' }),
+  year: z.number({
+    required_error: 'Year is required',
+    invalid_type_error: 'Year must be a number',
+  }).min(1900, { message: 'Year must be bigger or equal to 1900',
+  }).max(2022, { message: 'Year must be smaller or equal to 2022' }),
+  color: z.string({
+    required_error: 'Color is required',
+    invalid_type_error: 'Color must be a string',
+  }).min(3, { message: 'Color must be 3 or more characters long' }),
+  status: z.boolean({
+    invalid_type_error: 'Status can only be true or false',
+  }).optional(),
+  buyValue: z.number({
+    required_error: 'Value is required',
+    invalid_type_error: 'Value must be a number',
+  }).min(0, { message: 'Value must be bigger or equal to 0' }),
+});
+
+export type IVehicle = z.infer<typeof vehicleZodSchema>;
