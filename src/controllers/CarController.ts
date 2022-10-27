5c45dbf6a90e9962413233091a14981cad2df0b0
Joarez Ximenes <joarezximenesleitejunior@gmail.com>
2022-10-26 23:48:54 -0300

    Creating CarController

---
+++ src/controllers/CarController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { IService } from '../interfaces/IService';
+import { ICar } from '../interfaces/ICar';
+
+export default class CarController {
+  constructor(private _service: IService<ICar>) {}
+
+  public async create(req: Request, res: Response) {
+    const { model, year, color, buyValue, seatsQty, doorsQty } = req.body;
+    const car = { model, year, color, buyValue, seatsQty, doorsQty };
+
+    const createdCar = await this._service.create(car);
+
+    return res.status(201).json(createdCar);
+  }
+}
